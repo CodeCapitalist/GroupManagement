3274d836612e6df79d00815a59b1c045b6161383
chris.phillips <chris.phillips@jato.com>
2020-01-08 19:46:27 -0500

    trying again

---
+++ CodingMilitia.PlayBall.GroupManagement.Business.Impl/Services/InMemoryGroupsService.cs
@@ -0,0 +1,46 @@
+﻿using CodingMilitia.PlayBall.GroupManagement.Business.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodingMilitia.PlayBall.GroupManagement.Business.Models;
+
+namespace CodingMilitia.PlayBall.GroupManagement.Business.Impl.Services
+{
+    public class InMemoryGroupsService : IGroupsService
+
+    {
+        private readonly List<Group> _groups = new List<Group>();
+        private long _currentId = 0;
+        public IReadOnlyCollection<Group> GetAll()
+        {
+            return _groups.AsReadOnly();
+        }
+
+        public Group GetById(long id)
+        {
+            return _groups.SingleOrDefault(g => g.Id == id);
+        }
+
+        public Group Update(Group group)
+        {
+            var toUpdate = _groups.SingleOrDefault(g => g.Id == group.Id);
+            if (toUpdate == null)
+            {
+                return null;
+            }
+            else
+            {
+                toUpdate.Name = group.Name;
+                return toUpdate;
+            }
+        }
+
+        public Group Add(Group group)
+        {
+            group.Id = ++_currentId;
+            _groups.Add(group);
+            return group;
+        }
+    }
+}
---
+++ CodingMilitia.PlayBall.GroupManagement.Business/Services/IGroupsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodingMilitia.PlayBall.GroupManagement.Business.Models;
+
+namespace CodingMilitia.PlayBall.GroupManagement.Business.Services
+{
+    public interface IGroupsService
+    {
+        IReadOnlyCollection<Group> GetAll();
+        
+        Group GetById(long id);
+       
+        Group Update(Group group);
+        
+        Group Add(Group group);
+    }
+}
